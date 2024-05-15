80d56fb71055937d6d6a177ff442e228296202d0
Abhinav pandey <abhinav@smatbot.com>
2024-05-16 00:06:18 +0530

    'project-setup'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CreditCardDashboard';
+  sidebarOpen = true
+  togglesidebar(){
+    this.sidebarOpen = this.sidebarOpen? false : true;
+  }
+  
+
 }
